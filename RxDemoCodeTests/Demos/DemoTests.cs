0a523f93dc3c4d1e0bb3494ddddf0a4f858190a3
Niklas Dahlman <nd@sql8r.net>
2015-10-17 14:02:33 +0200

    nuget update

--- RxDemoCodeTests/Demos/DemoTests.cs
+++ RxDemoCodeTests/Demos/DemoTests.cs
@@ -19,5 +19,17 @@ namespace RxDemoCodeTests.Demos
 
            
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestDemo11()
+        {
+            Func<int, string> funUnderTest = x => (2*x).ToString();
+            //Arb.Default.Int32().Generator();
+            Func<int, Property> numProp = x => (x.ToString() == funUnderTest(x)).When(x % 3 != 0);
+            Prop.ForAll(numProp).QuickCheckThrowOnFailure();
+
+
+        }
     }
 }
